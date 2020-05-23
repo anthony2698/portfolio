080e1a84c870000fd3173184b2d6b9f7f1018f9b
anthony2698 <carrilloanthony2698@gmail.com>
2020-05-23 14:58:01 -0400

    Working on Navs.

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import Header from './components/Header';
 import Main from './pages/Main';
 import ProjectPage from './pages/ProjectsPage'
+import SkillPage from './pages/SkillPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import ResumePage from './pages/ResumePage';
 
-function App() {
+const App = () => {
     return (
         <div>
+            <Header />
             <Route exact path='/'>
                 <Main />
             </Route>
             <Route exact path='/projects'>
                 <ProjectPage />
+            </Route>
+            <Route path='/skills'>
+                <SkillPage/>
+            </Route>
+            <Route path='/about'>
+                <AboutPage/>
+            </Route>
+            <Route path='/contact'>
+                <ContactPage/>
+            </Route>
+            <Route path='/resume'>
+                <ResumePage/>
             </Route>
         </div>
     );
